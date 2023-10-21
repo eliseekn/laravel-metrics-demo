0cc64f0976f9b25c942669e882367dbcbdc2d88c
eliseekn <eliseekn@gmail.com>
2023-10-21 09:06:24 +0000

    Upgrade LaravelMetrics

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,28 +19,30 @@ class DashboardController extends Controller
         }
 
         return view('dashboard', [
-            'totalUsers' => $this->metrics(User::metrics()->count(), $period),
-            'totalProducts' => $this->metrics(Product::metrics()->count(), $period),
-            'totalOrders' => $this->metrics(Order::metrics()->count(), $period),
-            'usersTrends' => json_encode($this->trends(User::metrics()->count(), $period)),
-            'ordersTrends' => json_encode($this->trends(Order::metrics()->count(), $period)),
-            'ordersStatusTrends' => json_encode($this->trendsByStatus(Order::metrics()->count(), $period)),
-            'productsStatusTrends' => json_encode($this->trendsByStatus(Product::metrics()->count(), $period)),
+            'totalUsers' => $this->metrics(User::metrics(), $period),
+            'totalProducts' => $this->metrics(Product::metrics(), $period),
+            'totalOrders' => $this->metrics(Order::metrics(), $period),
+            'usersTrends' => json_encode($this->trends(User::metrics(), $period)),
+            'ordersTrends' => json_encode($this->trends(Order::metrics(), $period)),
+            'ordersStatusTrends' => json_encode($this->trendsByStatus(Order::metrics(), $period)),
+            'productsStatusTrends' => json_encode($this->trendsByStatus(Product::metrics(), $period)),
         ]);
     }
 
     private function metrics(LaravelMetrics $metrics, string|array $period): mixed
     {
         if (is_array($period)) {
-            return $metrics->between($period[0], $period[1])->metrics();
+            return $metrics
+                ->countBetween($period)
+                ->metrics();
         }
 
         return match($period) {
-            'day' => $metrics->byDay()->metrics(),
-            'week' => $metrics->byWeek()->metrics(),
-            'quater_year' => $metrics->byMonth(4)->metrics(),
-            'half_year' => $metrics->byMonth(6)->metrics(),
-            'month' => $metrics->byMonth()->metrics(),
+            'day' => $metrics->countByDay()->metrics(),
+            'week' => $metrics->countByWeek()->metrics(),
+            'quater_year' => $metrics->countByMonth(count: 4)->metrics(),
+            'half_year' => $metrics->countByMonth(count: 6)->metrics(),
+            'month' => $metrics->countByMonth()->metrics(),
         };
     }
 
@@ -48,32 +50,35 @@ class DashboardController extends Controller
     {
         if (is_array($period)) {
             return $metrics
-                ->between($period[0], $period[1])
+                ->countBetween($period)
                 ->fillEmptyDates()
                 ->trends();
         }
 
         return match($period) {
-            'day' => $metrics->byDay()->trends(),
-            'week' => $metrics->byWeek()->trends(),
-            'quater_year' => $metrics->byMonth(4)->trends(),
-            'half_year' => $metrics->byMonth(6)->trends(),
-            'month' => $metrics->byMonth()->trends(),
+            'day' => $metrics->countByDay()->trends(),
+            'week' => $metrics->countByWeek()->trends(),
+            'quater_year' => $metrics->countByMonth(count: 4)->trends(),
+            'half_year' => $metrics->countByMonth(count: 6)->trends(),
+            'month' => $metrics->countByMonth()->trends(),
         };
     }
 
     private function trendsByStatus(LaravelMetrics $metrics, string|array $period): array
     {
         if (is_array($period)) {
-            return $metrics->labelColumn('status')->between($period[0], $period[1])->trends();
+            return $metrics
+                ->labelColumn('status')
+                ->countBetween($period)
+                ->trends();
         }
 
         return match($period) {
-            'day' => $metrics->labelColumn('status')->byDay()->trends(),
-            'week' => $metrics->labelColumn('status')->byWeek()->trends(),
-            'quater_year' => $metrics->labelColumn('status')->byMonth(4)->trends(),
-            'half_year' => $metrics->labelColumn('status')->byMonth(6)->trends(),
-            'month' => $metrics->labelColumn('status')->byMonth()->trends(),
+            'day' => $metrics->labelColumn('status')->countByDay()->trends(),
+            'week' => $metrics->labelColumn('status')->countByWeek()->trends(),
+            'quater_year' => $metrics->labelColumn('status')->countByMonth(count: 4)->trends(),
+            'half_year' => $metrics->labelColumn('status')->countByMonth(count: 6)->trends(),
+            'month' => $metrics->labelColumn('status')->countByMonth()->trends(),
         };
     }
 }
